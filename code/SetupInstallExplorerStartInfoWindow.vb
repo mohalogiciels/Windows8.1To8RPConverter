a5227300e991e8e890bc11a70a55032ea5289bbb
Moha Logiciels <mohalogiciels@hotmail.com>
2026-02-12 15:04:07 +0100

    Delete code/SetupInstallExplorerStartInfoWindow.vb

--- code/SetupInstallExplorerStartInfoWindow.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class SetupInstallExplorerStartInfoWindowEnglish
-
-    Private Sub SetupInstallExplorerStartInfoWindowEnglish_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
-        e.Cancel = True
-    End Sub
-End Class
