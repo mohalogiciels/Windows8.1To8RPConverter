4190e29aad40e21f2d7ea6cf3fcb4d0f5440513e
Moha Logiciels <mohalogiciels@hotmail.com>
2026-02-12 15:03:56 +0100

    Delete code/ModifyModifyExplorerStartInfoWindow.vb

--- code/ModifyModifyExplorerStartInfoWindow.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class ModifyModifyExplorerStartInfoWindowEnglish
-
-    Private Sub ModifyModifyExplorerStartInfoWindowEnglish_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
-        e.Cancel = True
-    End Sub
-End Class
